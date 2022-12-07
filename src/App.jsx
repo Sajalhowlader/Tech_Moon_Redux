e63ee09b5fd9fc3a02a60d35378a9c16a9300312
@ ANTU <sajolhowlader08@gmail.com>
2022-12-07 11:21:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <p>Hello Redux</p>
+    </div>
+  );
+}
+
+export default App;
